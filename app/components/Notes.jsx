252779f9c0457257280fd001d6e0188975cc85a7
WalterY <yanez.walt@gmail.com>
2023-05-09 19:45:33 -0300

    Components

---
+++ app/components/Notes.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+/* import uuid from 'uuid'; */
+
+/*
+    Utilizaremos una implementación de Node.js conocida como uuid y su variante uuid.v4,
+    que nos dara ids tales 1c8e7a12-0b4c-4f23-938c-00d7161f94fc que, casi con toda seguridad,
+    serán únicos.
+
+    const notes = [
+    {
+        /* id : '4e81fc6e-bfb6-419b-93e5-0242fb6f3f6a', 
+        id: uuid.v4(),
+        task: 'Learn React'
+    },
+    {
+        /* id: '11bbffc8-5891-4b45-b9ea-5c99aadf870f', 
+        id: uuid.v4(),
+        task: 'Do laundry'
+    }
+    ];
+*/
+
+/* 
+    .map devuelve una lista de elemento li para React los renderice 
+
+    Usamos la propiedad key para poder decirte a React que items han sido cambiados, modificados o borrados.
+    Es importante que sea único ya que, sino, React no será capaz de saber el orden correcto en el que debe 
+    renderizarlos. 
+
+*/
+export default ({notes}) => (
+    <ul>{notes.map(note =>
+        <li key={note.id}>{note.task}</li>
+    )}</ul>
+)
+
